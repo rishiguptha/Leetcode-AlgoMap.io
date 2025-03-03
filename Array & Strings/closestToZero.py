0442ea2df7a7e8a71aea0a69ff31a4d044409e14
rishiguptha <guptamankala@gmail.com>
2025-03-03 17:34:35 -0500

    Implement findClosestNumber method to return the integer closest to zero in a list

---
+++ Array & Strings/closestToZero.py
@@ -0,0 +1,14 @@
+# Given an integer array nums of size n, return the number with the value closest to 0 in nums. 
+# If there are multiple answers, return the number with the largest value.
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        for i in nums:
+            if abs(i) < abs(closest):
+                closest = i
+        
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
